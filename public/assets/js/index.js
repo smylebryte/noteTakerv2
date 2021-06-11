3a1732bda9cec9a4e0de0be1b651196de937bb68
Smylebryte <aliciahughes1446@gmail.com>
2021-06-11 12:36:23 -0500

    finished apiRoutes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -13,12 +13,12 @@ if (window.location.pathname === '/notes') {
 }
 
 // Show an element
-const show = (elem) => {
+const show = elem => {
   elem.style.display = 'inline';
 };
 
 // Hide an element
-const hide = (elem) => {
+const hide = elem => {
   elem.style.display = 'none';
 };
 
@@ -33,7 +33,7 @@ const getNotes = () =>
     },
   });
 
-const saveNote = (note) =>
+const saveNote = note =>
   fetch('/api/notes', {
     method: 'POST',
     headers: {
@@ -42,7 +42,7 @@ const saveNote = (note) =>
     body: JSON.stringify(note),
   });
 
-const deleteNote = (id) =>
+const deleteNote = id =>
   fetch(`/api/notes/${id}`, {
     method: 'DELETE',
     headers: {
@@ -76,7 +76,7 @@ const handleNoteSave = () => {
 };
 
 // Delete the clicked note
-const handleNoteDelete = (e) => {
+const handleNoteDelete = e => {
   // prevents the click listener for the list from being called when the button inside of it is clicked
   e.stopPropagation();
 
@@ -94,14 +94,14 @@ const handleNoteDelete = (e) => {
 };
 
 // Sets the activeNote and displays it
-const handleNoteView = (e) => {
+const handleNoteView = e => {
   e.preventDefault();
   activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
   renderActiveNote();
 };
 
 // Sets the activeNote to and empty object and allows the user to enter a new note
-const handleNewNoteView = (e) => {
+const handleNewNoteView = e => {
   activeNote = {};
   renderActiveNote();
 };
@@ -115,10 +115,10 @@ const handleRenderSaveBtn = () => {
 };
 
 // Render the list of note titles
-const renderNoteList = async (notes) => {
+const renderNoteList = async notes => {
   let jsonNotes = await notes.json();
   if (window.location.pathname === '/notes') {
-    noteList.forEach((el) => (el.innerHTML = ''));
+    noteList.forEach(el => (el.innerHTML = ''));
   }
 
   let noteListItems = [];
@@ -155,7 +155,7 @@ const renderNoteList = async (notes) => {
     noteListItems.push(createLi('No saved Notes', false));
   }
 
-  jsonNotes.forEach((note) => {
+  jsonNotes.forEach(note => {
     const li = createLi(note.title);
     li.dataset.note = JSON.stringify(note);
 
@@ -163,7 +163,7 @@ const renderNoteList = async (notes) => {
   });
 
   if (window.location.pathname === '/notes') {
-    noteListItems.forEach((note) => noteList[0].append(note));
+    noteListItems.forEach(note => noteList[0].append(note));
   }
 };
 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,27 +2,53 @@ const express = require('express');
 const router = require('express').Router();
 const db = require('../db/db');
 const fs = require('fs');
-const { uuid } = require('uuidv4');
+var uniqid = require('uniqid');
+// const { uuid } = require('uuidv4');
 
 router.get('/api/notes', (req, res) => {
-  fs.readFile('./db/db.json', (err, data) => {
-    res.send(data);
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    res.json(JSON.parse(data));
   });
 });
 
 router.post('/api/notes', (req, res) => {
-  const newNote = req.body;
-  fs.writeFile('./db/db.json', function (err, newNote) {
-    console.log('you did it!');
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    const notesDB = JSON.parse(data);
+    const newNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uniqid(),
+    };
+    notesDB.push(newNote);
+    fs.writeFile('./db/db.json', JSON.stringify(notesDB), err => {
+      if (err) console.log(err);
+      res.json(data);
+    });
   });
 });
 
 router.delete('/api/notes/:id', (req, res) => {
   const noteId = req.params.id;
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    const notesDB = JSON.parse(data);
+    for (let i = 0; i < notesDB.length; i++) {
+      if (noteId === notesDB[i].id) {
+        notesDB.splice([i], 1);
+        fs.writeFile('./db/db.json', JSON.stringify(notesDB), err => {
+          if (err) console.log(err);
+          res.json(data);
+        });
+      }
+    }
+  });
 });
 
 module.exports = router;
 
+// const newNote = req.body;
+// fs.writeFile('./db/db.json', JSON.stringify(newNote), err => {
+//   if (err) console.log(err);
+// });
 // router.get('/api/notes', (req, res) => {
 //   let dbNotes = JSON.parse(fs.readFile('../db/db.json', 'utf-8'));
 //   return res.json(dbNotes);
@@ -32,3 +58,18 @@ module.exports = router;
 
 //   return res.json(notes);
 // });
+
+// [
+//   {
+//     "title": "Test Title",
+//     "text": "Test text"
+//   },
+//   {
+//     "title": "Note 2",
+//     "text": "Note text 2"
+//   },
+//   {
+//     "title": "Note 3",
+//     "text": "Note text 3"
+//   }
+// ]
